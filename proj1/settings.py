71e82f162463d7be25be352042a3142a8eddfd1b
LeonardoMarquis <marquisleonardo12@gmail.com>
2025-12-20 15:43:39 -0300

    settings debug true

--- proj1/settings.py
+++ proj1/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qktjrytuw@guxidyml-6v&#evl+1o#bv@mm25au=dr6_g=)*i%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ex1-django.onrender.com', 'localhost', '127.0.0.1']       # dominio apos o deploy no render por ex.
 
